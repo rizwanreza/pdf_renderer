95ba9c1fab90e9546382c81bb5b1613b8b53cd6c
Rizwan Reza <rizwanreza@gmail.com>
2012-03-08 11:52:57 +0330

    Inline formatting should be enabled

--- lib/pdf_renderer.rb
+++ lib/pdf_renderer.rb
@@ -4,9 +4,9 @@ require "prawn"
 Mime::Type.register "application/pdf", :pdf
 ActionController::Renderers.add :pdf do |filename, options|
   pdf = Prawn::Document.new
-  pdf.text render_to_string(options) 
+  pdf.text render_to_string(options), :inline_format => true
   send_data(pdf.render, :filename => "#{filename}.pdf",
-                        :type => "application/pdf", 
+                        :type => "application/pdf",
                         :disposition => "attachment")
 end
 
